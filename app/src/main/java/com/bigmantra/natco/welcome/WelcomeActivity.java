2af1083207774b75a1aa0dd0c461416769c2ea80
Girish Lakshmanan <girish.la@gmail.com>
2019-04-23 22:58:19 +0100

    corrected style issues

--- app/src/main/java/com/bigmantra/natco/welcome/WelcomeActivity.java
+++ app/src/main/java/com/bigmantra/natco/welcome/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 import com.bigmantra.natco.R;
@@ -27,6 +28,9 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_activity);
         ButterKnife.bind(this);
+
+
+        signupButton.setVisibility(View.GONE);
 
         signupButton.setOnClickListener(v -> {
             SignUpActivity.newInstance(this);
